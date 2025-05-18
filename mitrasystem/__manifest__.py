f27b86ef41af65b73daa2de2e087e729e3f92c6b
Novelya Putri Ramadhani <114747134+novelxv@users.noreply.github.com>
2025-05-18 19:16:40 +0700

    feat: init module

---
+++ mitrasystem/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'MitraSystem',
+    'version': '1.0',
+    'summary': 'Sistem Manajemen Proyek dan Alur Kerja Terintegrasi',
+    'category': 'Project',
+    'author': 'MitraSystem',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
